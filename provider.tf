0202dbaeb383f921c8bde8e3193e02de5a076b0c
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2023-11-29 09:41:41 +0000

    upgraded provider

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.87.0"
+      version = "~>2.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
